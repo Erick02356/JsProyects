c3e400b8ab01d22e30045d2fb28cd1a9df1877eb
Erick <eick02356@gmail.com>
2024-07-29 19:52:28 -0500

    Intentos de avance con el guardado de numeros a operar

--- 1. Calculadora/index.js
+++ 1. Calculadora/index.js
@@ -2,6 +2,7 @@
 //QuerySelctor, agregamos un evento que cada ver que se de clic en un de sus elementos hijos podremos ver el contenido
 const teclas= document.querySelector('.teclas')
 const pantalla = document.getElementById('numero')
+
 /*El evento addEventListener Solo puede funcionar si es puesto
 en un querySelector o en getElementById. Supongo que no funciona
 en metodos que devuelven un listado de funciones
@@ -20,12 +21,16 @@ teclas.addEventListener('click', e=> {
       el numero 0 cuenta con dos estilos diferentes no valida
       entonces se agrego la segunda parte del condicional
     */
-    if (claseNumeros == 'numeros' || numeros ==0){
+    if (claseNumeros == 'numeros' || numeros ==0  ){
         if (numeroEnPantalla == 0 ){
             pantalla.textContent=numeros;
+        
         } else{
             pantalla.textContent=numeroEnPantalla+numeros
+            let numero1 = numeroEnPantalla
+          
         }
+
     }
     if (claseNumeros == 'decimal'){
         
@@ -38,9 +43,19 @@ teclas.addEventListener('click', e=> {
 
         }
     }
+    if (claseNumeros == 'operador'  || claseNumeros== 'boton-largo' ){
+        pantalla.textContent =0
+        action.primerValor = pantalla.textContent
+        console.log(action.primerValor)
+    }
 
+    if (claseNumeros == 'calcular'){       
+        let numero2=numeroEnPantalla
+        guardarNumero(numero2)
+        console.log(guardarNumero)
+    }
+  
 
-    
 })
 // Se llama el id del elemento AC para agregar un eventListener el cual
 // cada que se de click automaticamente reiniciara la pantalla en 0
@@ -49,7 +64,20 @@ eliminar.addEventListener('click',x =>{
    pantalla.textContent=0
 }  )
 
+function suma (a,b){
+    resultado = parseFloat(a)+ parseFloat(b)
+    console.log(resultado)
+}
+const sumar = document.getElementById('sumar')
+const restar = document.getElementById('restar')
+const multiplicar = document.getElementById('multiplicar')
+const dividir =document.getElementById('dividir')
 
+function guardarNumero(a){
+    operacion=[]
+    operacion.push(a)
+    return operacion
+}
 // const punto = document.getElementById('decimal')
 // punto.addEventListener('click', e=>{
 //     pantalla.textContent=pantalla.textContent+'.'  
--- 1. Calculadora/pasosCreacion.md
+++ 1. Calculadora/pasosCreacion.md
@@ -6,7 +6,17 @@ Pasos  a seguir,
 2.	Que limpie la pantalla
 
 3.	Que agregue un punto decimal si así se desea (no permitir más de un punto)
-4.	Identificar el operador arimetico
+
+4.	Identificar el operador 
+Al identificar el operador surgen dudas de como se podría proceder pues:
+ a. guarda el numero ya escrito, luego agrega el operador para finalmente esperar que se escriba el siguiente numero y
+ así asignarlo en otra variable
+    $capturo el valor1
+    $Lo guardo en una variable
+    $le hago push en un arreglo
+    $luego limpio la pantalla
+    $Agrego el otro numero
 5.	Agregar el segundo numero
 6.	Calcular
 7.	DISFRUTAR
+
