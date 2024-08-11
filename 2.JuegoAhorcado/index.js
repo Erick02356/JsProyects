cfd707e7e9cbc88825acef27b9be0e9f1c73ae94
Erick <eick02356@gmail.com>
2024-08-10 21:32:46 -0500

    Se agrego los espacios a completar, cuando se oprime una tecla que coincide con la palabra se muestra en pantalla, falta agregar la logica cuando no se cumple

--- 2.JuegoAhorcado/index.js
+++ 2.JuegoAhorcado/index.js
@@ -1,8 +1,38 @@
-const palabras = ['casa']
+const palabras = ['perro']
 const letras = document.querySelector('.teclado')
-console.log(letras)
+
+/*Esta variable se hace con el fin de que el usuario pueda 
+de manera dinamica escoger que palabra quiere adiviar del
+banco de palabras*/
+let numeroPalabraAdivinar=0
+//console.log(letras)
+
+const espacios = []
+const intentos = 10 + palabras[numeroPalabraAdivinar].length
+console.log(intentos)
+for (let i = 0; i < palabras[numeroPalabraAdivinar].length; i++) {
+    espacios.push(' _ ')
+}
+// console.log(espacios)
+const palabra = document.getElementById('palabra')
+palabra.innerHTML=espacios.join('');
+//console.log(element)
+// console.log()
+const palabraAdivinar = Array.from(palabras[numeroPalabraAdivinar])
+
 letras.addEventListener('click', e=> {
     const letra = e.target.textContent
-    console.log(letra)
-
+    //console.log(letra)
+    for (let index = 0; index < intentos; index++) {
+        
+        if (palabraAdivinar[index] == letra) {
+            espacios[index]=letra
+            palabra.innerHTML=espacios.join('');
+            // console.log('Si es')
+        } else { 
+            // console.log('No es')            
+        }
+        
+    }
 });
+
